218fee54df86eb10633888a5022a42834443d7ec
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-17 14:57:14 +0200

    fix: from card to table

--- resources/views/dc_comics/index.blade.php
+++ resources/views/dc_comics/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('content')
+{{-- @section('content')
     <div class="container mt-5">
         <div class="row g-3">
             @foreach ($comicArray as $comic)
@@ -16,4 +16,34 @@
             @endforeach
         </div>
     </div>
+@endsection --}}
+
+@section('content')
+    <table class="table">
+        <thead>
+            <tr class="text-center">
+                <th scope="col">Copertina</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Serie</th>
+                <th scope="col">Tipo</th>
+                <th scope="col">Data di uscita</th>
+                <th scope="col">Prezzo</th>
+                <th scope="col">Dettagli</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comicArray as $comic)
+                <tr class="text-center align-middle">
+                    <th><img src="{{$comic->thumb}}" alt="{{$comic->title}}"></th>
+                    <td>{{$comic->title}}</td>
+                    <td>{{$comic->series}}</td>
+                    <td>{{$comic->type}}</td>
+                    <td>{{$comic->sale_date}}</td>
+                    <td>{{$comic->price}}</td>
+                    <td><button class="btn btn-primary"><a class="text-white" href="{{route('dc-comics.show', ['dc_comic' => $comic->id]) }}}}">Dettagli</a></button></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection

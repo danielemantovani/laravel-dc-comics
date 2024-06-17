f15ad75dfd6b1e627798878a5f81044e6696caa3
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-17 15:40:14 +0200

    fix: color title show.blade

--- resources/views/dc_comics/show.blade.php
+++ resources/views/dc_comics/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mt-5">
-        <h2 class="text-center mb-5">Dettagli {{$dcComic->title}}</h2>
+        <h3 class="text-center mb-5">Dettagli <span class="text-primary">{{$dcComic->title}}</span></h3>
         <dl>
             <dt>
                 <dd><h5>Anno di uscita:</h5>{{($dcComic->sale_date)}}</dd>
